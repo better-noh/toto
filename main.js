516fa3d72b8b937bfaeccd039da99cc7ccfbeb87
MinziiK <zxcv923111@gmail.com>
2024-03-01 23:10:40 +0900

    memo test

--- main.js
+++ main.js
@@ -1,21 +1,17 @@
-// const todo = document.querySelector();
-
 let calendar;
 
 document.addEventListener('DOMContentLoaded', function() {
+    // 달력 초기화
     let calendarEl = document.getElementById('calendar');
     calendar = new FullCalendar.Calendar(calendarEl, {
         initialView: 'dayGridMonth',
         selectable: true,
         events: [],
-
         dateClick: function (e) {
             let eventTitle = prompt("Enter event title:");
-            if(eventTitle===null || eventTitle===""){ return }
+            if(eventTitle === null || eventTitle === ""){ return; }
             addEventToCalendar({ title:eventTitle, start:e.dateStr, color:'#8b008b' });
-            // removeEventFromCalendar(e.dateStr);
         }
-
     });
     calendar.render();
 });
