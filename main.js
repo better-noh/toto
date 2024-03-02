5d0c763751d4e9e6a3546d8461a3d743af2762ff
MinziiK <zxcv923111@gmail.com>
2024-03-02 21:07:45 +0900

    add modal

--- main.js
+++ main.js
@@ -1,28 +1,36 @@
 let calendar;
+let selectedDate = null;
 
 document.addEventListener('DOMContentLoaded', function() {
-    // 달력 초기화
     let calendarEl = document.getElementById('calendar');
     calendar = new FullCalendar.Calendar(calendarEl, {
         initialView: 'dayGridMonth',
         selectable: true,
         events: [],
-        dateClick: function (e) {
-            let eventTitle = prompt("Enter event title:");
-            if(eventTitle === null || eventTitle === ""){ return; }
-            addEventToCalendar({ title:eventTitle, start:e.dateStr, color:'#8b008b' });
+        select: function(info) {
+            selectedDate = info.startStr; // 선택한 날짜 저장
+            $('#memoModal').modal('toggle'); // 모달 표시
         }
     });
     calendar.render();
-});
 
+    document.getElementById('memoForm').addEventListener('submit', function(e) {
+        e.preventDefault(); // 폼 기본 제출 동작 방지
+        let eventTitle = document.getElementById('memoText').value.trim();
+
+        if(eventTitle) { // 제목이 비어있지 않은 경우에만 이벤트 추가
+            addEventToCalendar({
+                title: eventTitle,
+                start: selectedDate,
+                color: '#8b008b'
+            });
+        }
+
+        $('#memoModal').modal('hide'); // 모달 숨기기
+        document.getElementById('memoForm').reset(); // 폼 내용 초기화
+    });
+});
 
 function addEventToCalendar(event){
     calendar.addEvent(event);
 }
-
-function removeEventFromCalendar(id){
-    let calendarEvent = calendar.getEventById(id);
-    calendarEvent.remove();
-}
-
